b9c81a9bd68e4eae1982faaa557ecbbc02f5715b
mpawlowski98 <107926477+mpawlowski98@users.noreply.github.com>
2023-05-24 09:03:17 +0000

    Deploying to gh-pages from @ mpawlowski98/goit-react-hw-08-phonebook@0a7fa23e8f88c0ddce1228d8eac7899afb4248f1 🚀

---
+++ static/js/266.5fd22a6e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,n,r){r.r(n),r.d(n,{default:function(){return a}});var t=r(184);function a(){return(0,t.jsx)("div",{children:(0,t.jsxs)("h1",{children:["Contacts manager welcome page"," ",(0,t.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=266.5fd22a6e.chunk.js.map
